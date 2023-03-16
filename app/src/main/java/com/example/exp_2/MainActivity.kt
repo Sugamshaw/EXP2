3f92a6c2e99b4ceafe2361902137b5cfff663307
SUGAM <sugamshaw2002@gmail.com>
2023-03-16 10:28:12 +0530

    exp-01---1_commit

--- app/src/main/java/com/example/exp_2/MainActivity.kt
+++ app/src/main/java/com/example/exp_2/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        println("please do it")
         println("second commit")
         println("we are in experimental branch")
     }
